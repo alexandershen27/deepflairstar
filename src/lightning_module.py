424e2147fe954531203335be9d317002d8f53ade
Alexander Shen <alshen@gmail.com>
2026-03-09 23:25:42 -0700

    Research Mode: Implemented 3 distinct Swin variants as separate files for parallel comparison

--- src/lightning_module.py
+++ src/lightning_module.py
@@ -4,8 +4,12 @@ import pytorch_lightning as pl
 import os
 import matplotlib.pyplot as plt
 from monai.inferers import SlidingWindowInferer
+
+# Multi-Model Imports
 from src.arch.unet import DeepFLAIRNet
-from src.arch.swin import DeepFLAIRSwin
+from src.arch.swin_instance import DeepFLAIRSwinInstance
+from src.arch.swin_batch_sig import DeepFLAIRSwinBatchSig
+from src.arch.swin_batch_hard import DeepFLAIRSwinBatchHard
 from src.losses import DeepFLAIRLoss
 
 class DeepFLAIRLightningModule(pl.LightningModule):
@@ -24,13 +28,13 @@ class DeepFLAIRLightningModule(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters()
         
-        if model_type == "swin":
-            self.model = DeepFLAIRSwin(
-                img_size=patch_size,
-                in_channels=1,
-                out_channels=1,
-                feature_size=base_channels
-            )
+        # Architecture Selection
+        if model_type == "swin_instance":
+            self.model = DeepFLAIRSwinInstance(feature_size=base_channels, img_size=patch_size)
+        elif model_type == "swin_batch_sig":
+            self.model = DeepFLAIRSwinBatchSig(feature_size=base_channels, img_size=patch_size)
+        elif model_type == "swin_batch_hard":
+            self.model = DeepFLAIRSwinBatchHard(feature_size=base_channels, img_size=patch_size)
         else:
             self.model = DeepFLAIRNet(base_channels=base_channels)
             
@@ -56,57 +60,42 @@ class DeepFLAIRLightningModule(pl.LightningModule):
         x, y = batch["image"], batch["label"]
         y_hat = self.model(x)
         loss, metrics = self.loss_fn(y_hat, y)
-        
         bs = x.shape[0]
         self.log("train_loss", loss, prog_bar=True, on_step=True, on_epoch=True, sync_dist=True, batch_size=bs)
         self.log("train_l1", metrics["l1"], sync_dist=True, batch_size=bs)
-        self.log("train_ssim_loss", metrics["ssim_loss"], sync_dist=True, batch_size=bs)
-        self.log("train_grad_loss", metrics["grad_loss"], sync_dist=True, batch_size=bs)
-            
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch["image"], batch["label"]
         y_hat = self.inferer(x, self.model)
         loss, metrics = self.loss_fn(y_hat, y)
-        
         bs = x.shape[0]
         self.log("val_loss", loss, prog_bar=True, sync_dist=True, batch_size=bs)
         self.log("val_l1", metrics["l1"], sync_dist=True, batch_size=bs)
-        self.log("val_ssim_loss", metrics["ssim_loss"], sync_dist=True, batch_size=bs)
-        self.log("val_grad_loss", metrics["grad_loss"], sync_dist=True, batch_size=bs)
-            
         if batch_idx == 0:
             self._log_images(x, y, y_hat, "val")
-            
         return loss
 
     def _log_images(self, x, y, y_hat, stage):
         if not self.trainer.is_global_zero:
             return
-            
         img_vol = x[0, 0].detach().cpu().numpy()
         lbl_vol = y[0, 0].detach().cpu().numpy()
         pred_vol = y_hat[0, 0].detach().cpu().numpy()
-        
         c = [s // 2 for s in img_vol.shape]
         views = [
             (img_vol[c[0], :, :], lbl_vol[c[0], :, :], pred_vol[c[0], :, :], "Axial"),
             (img_vol[:, c[1], :], lbl_vol[:, c[1], :], pred_vol[:, c[1], :], "Sagittal"),
             (img_vol[:, :, c[2]], lbl_vol[:, :, c[2]], pred_vol[:, :, c[2]], "Coronal")
         ]
-        
         fig, axes = plt.subplots(3, 3, figsize=(15, 15))
         for i, (img, lbl, pred, title) in enumerate(views):
-            axes[i, 0].imshow(img, cmap='gray'); axes[i, 0].set_title(f"Input {title}")
-            axes[i, 1].imshow(lbl, cmap='gray'); axes[i, 1].set_title(f"Target {title}")
-            axes[i, 2].imshow(pred, cmap='gray'); axes[i, 2].set_title(f"Pred {title} (E{self.current_epoch:03d})")
+            axes[i, 0].imshow(img, cmap='gray'); axes[i, 0].set_title(f"In {title}")
+            axes[i, 1].imshow(lbl, cmap='gray'); axes[i, 1].set_title(f"GT {title}")
+            axes[i, 2].imshow(pred, cmap='gray'); axes[i, 2].set_title(f"Pred {title}")
             for ax in axes[i]: ax.axis('off')
-            
         plt.tight_layout()
         epoch_path = f"vis/epoch_{self.current_epoch:03d}_{stage}_3view.png"
-        pred_only_path = f"vis/epoch_{self.current_epoch:03d}_pred.png"
-        
         plt.savefig(epoch_path)
         plt.close(fig)
         
@@ -115,13 +104,7 @@ class DeepFLAIRLightningModule(pl.LightningModule):
                 run_id = logger.run_id if hasattr(logger, "run_id") else None
                 if run_id:
                     logger.experiment.log_artifact(run_id=run_id, local_path=epoch_path, artifact_path="visualizations")
-                else:
-                    logger.experiment.log_artifact(local_path=epoch_path, artifact_path="visualizations")
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.model.parameters(), 
-            lr=self.hparams.lr,
-            betas=(self.hparams.beta1, self.hparams.beta2)
-        )
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.lr, betas=(self.hparams.beta1, self.hparams.beta2))
         return optimizer
